1c73e3ee6e8634099ad88667caf5be57321cb396
thndrbrrr <thndrbrrr@gmail.com>
2025-10-19 12:43:51 -0700

    Add license information

--- gtasks-org-tools.el
+++ gtasks-org-tools.el
@@ -1,9 +1,22 @@
 ;;; gtasks-org-tools.el --- Google Tasks Import and Export -*- lexical-binding: t; -*-
 ;;; Commentary:
-;;    TODO
 ;;
-;; Usage:
-;;   TODO
+;; Import Google Tasks into Org and export Org entries to Google Tasks.
+;;
+;; Copyright (C) 2025 thndrbrrr@gmail.com
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ;;
 ;;; Code:
 
@@ -187,12 +200,12 @@ DEADLINEs or timestamps."
 (defun gtasks-org-tools-pull (tasklist-id file &rest post-import-action)
   "Fetch tasks for TASKLIST-ID, append to FILE, and optionally post-process them.
 
-POST-IMPORT-ACTION (optional) controls what to do with imported tasks:
-  - nil (default)      :  do nothing.
-  - 'complete          :  mark each imported task completed.
-  - 'delete            :  delete each imported task.
+Runs `gtasks-org-tools-after-append-hook' before post-import-action.
 
-Runs gtasks-org-tools-after-append-hook before post-import-action.
+POST-IMPORT-ACTION (optional) controls what to do with imported tasks:
+  - nil (default):  do nothing.
+  - 'complete    :  mark each imported task completed.
+  - 'delete      :  delete each imported task.
 
 Returns t if entries were successfully appended."
   (let* ((action (car post-import-action)))
